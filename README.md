f1fe9d6404e99a092ce063f95e5a9e8155069785
mballoni <mballoni@ciandt.com>
2016-03-09 17:07:04 -0300

    [mballoni] - Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# gatling-maven
+
+##generated using maven archetype:
+http://gatling.io/docs/2.1.7/extensions/maven\_archetype.html
+
+##Execute using:
+mvn gatling:execute
+
+##Another example:
+https://github.com/gatling/gatling-maven-plugin-demo
+
+##Maven gatling plugin configuration:
+http://gatling.io/docs/2.1.6/extensions/maven\_plugin.html
+
+##Env:
+http://gatling.io/docs/1.5.6/developing\_gatling/development\_environment.html#dev-env
+
+
---
+++ src/test/scala/stock/poc/stock/StockSimulation.scala
@@ -0,0 +1,32 @@
+package product;
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+import io.gatling.http.config.HttpProtocolBuilder.toHttpProtocol
+import io.gatling.http.request.builder.HttpRequestBuilder.toActionBuilder
+
+class StockSimulation extends Simulation {
+
+  val httpConf = http
+    .baseURL("http://localhost:8080/api/transaction/")
+    .acceptHeader("text/html,application/xhtml+xml,application/json;q=0.9,*/*;q=0.8")
+    .doNotTrackHeader("1")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+
+  val feeder = csv("search.csv").circular
+
+  val scn = scenario("StockSimulation")
+  .during(100){
+     feed(feeder)
+    .exec(http("stock_operation")
+    .get("${sku}/${size}")
+    .check(status.is(200))
+    .check(bodyString.is("${sku}")))
+	}
+  setUp(
+    scn.inject(atOnceUsers(100))
+  ).protocols(httpConf)
+}
